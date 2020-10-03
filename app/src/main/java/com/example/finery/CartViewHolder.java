dab3962c67facd5180b842df8ee032f04375759f
perusha <perushawanniarachchi22@gmail.com>
2020-10-03 11:02:04 +0530

    peru4 commit

--- app/src/main/java/com/example/finery/CartViewHolder.java
+++ app/src/main/java/com/example/finery/CartViewHolder.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.rey.material.widget.Spinner;
+
 public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     public TextView txtProductName,txtProductPrice,txtProductQuantity;
