a2df10e3f77c0a0776857120aeabf475dd9d3ed9
Dulya Perera <dulyakemali@gmail.com>
2020-10-04 18:12:40 +0530

    model changes

--- app/src/main/java/com/example/finery/Feedback.java
+++ app/src/main/java/com/example/finery/Feedback.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.finery.Model.Feed;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
--- app/src/main/java/com/example/finery/Model/Cart.java
+++ app/src/main/java/com/example/finery/Model/Cart.java
@@ -1,4 +1,4 @@
-package com.example.finery;
+package com.example.finery.Model;
 
 public class Cart {
     private String pid,pname,price, quantity;
